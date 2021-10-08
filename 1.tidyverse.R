c34d74988e1ff9aa135742c815dc9db20ddd8259
Luca Biavati <lbiavat1@jh.edu>
2021-10-08 17:51:50 -0400

    tidyverse chapter!

--- 1.tidyverse.R
+++ 1.tidyverse.R
@@ -62,3 +62,38 @@ spread(tidyptData, key = Month, value = BMI)
 # gather vs, am, gear, carb variables from mtcars into a single key-value pair
 mtcars
 gathermtcars <- gather(mtcars, key = "variable", value = "value", c(vs, am, gear, carb))
+
+# purrr
+
+mtcars
+map_dbl(mtcars, sum)
+map_lgl(mtcars, ~sum(.) > 100)
+map_lgl(mtcars, function(.) sum(.) > 100)
+
+listOfNumerics <- list( a = rnorm(10),
+                        b = rnorm(100),
+                        c = rnorm(1000))
+par(mfrow = c(1,3))
+
+# walk() - apply a function to each element of a list to produce the function's side effects
+walk(listOfNumerics, hist)
+
+# iwalk() - makes name/index of each element available. 
+# use .x to reference the list element and .y to reference its name/index
+iwalk(listOfNumerics, ~ hist(.x, main = .y))
+
+# iterating over multiple lists simultaneously
+
+multipliers <- list(0.5, 10, 3)
+
+map2(.x = listOfNumerics, .y = multipliers, ~.x * .y)
+
+# use pmap()
+args <- expand.grid(n = c(1000, 10000, 1000000),
+                    mean = c(1),
+                    sd = c(0.1, 1, 10))
+args
+par(mfrow = c(3,3))
+
+pmap(args, rnorm) %>%
+  iwalk(~hist(.x, main = paste("Element", .y, sep = "_")))
