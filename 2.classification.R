c8ab487f3366b12e7a6691c71644c69c11f22e54
Luca Biavati <lbiavat1@jh.edu>
2021-10-11 11:42:43 -0400

    install "mlr" too

--- 2.classification.R
+++ 2.classification.R
@@ -3,7 +3,26 @@ rm(list = ls())
 # install mlr3 package
 if(!require(mlr3))
   install.packages("mlr3")
+if(!require(mlr))
+  install.packages("mlr")
 
 library(mlr3)
+library(mlr) #older version - used in the book
 library(tidyverse)
+
+data(diabetes, package = "mclust")
+dbTib <- as_tibble(diabetes)
+dbTib
+
+summary(dbTib)
+
+ggplot(dbTib, aes(glucose, insulin, shape = class, col = class)) +
+  geom_point() +
+  theme_bw() 
+ggplot(dbTib, aes(glucose, sspg, col = class)) +
+  geom_point() +
+  theme_bw()
+ggplot(dbTib, aes(insulin, sspg, col = class)) +
+  geom_point() +
+  theme_bw()
 
